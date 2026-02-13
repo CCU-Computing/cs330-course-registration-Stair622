bb1fd4e945e16b617bfe4d37bff5fee9d1b89ce5
Jordan Stair <jdstair@coastal.edu>
2026-02-12 19:38:02 -0500

    completed user story 1

--- CourseServices.cs
+++ CourseServices.cs
@@ -8,7 +8,7 @@ namespace cs330_proj1
         private CourseRepository repo = new CourseRepository();
 
 
-        //As a student, I want to search for course offerings that meet core goals 
+         //As a student, I want to search for course offerings that meet core goals 
         // so that I can register easily for courses that meet my program requirements
          public List<CourseOffering> getOfferingsByGoalIdAndSemester(String theGoalId, String semester) {
           //finish this method during the tutorial 
@@ -25,7 +25,7 @@ namespace cs330_proj1
             }
          }
          if(theGoal==null) throw new Exception("Didn't find the goal");
-            //search list of courses, then for each course, search offerings
+         //search list of courses, then for each course, search offerings
        List<CourseOffering> courseOfferingsThatMeetGoal = new List<CourseOffering>();
             
       foreach(CourseOffering c in theOfferings) {
@@ -38,29 +38,7 @@ namespace cs330_proj1
       }
        return courseOfferingsThatMeetGoal;
         }
-
-        
-        //Add more service functions here, as needed, for the project
-
-        /* As a student, I want to see all available courses so that I know what my options are */
-
-        /* As a student, I want to see all course offerings by semester, so that I can choose from what's
-           available to register for next semester */
-
-        /* As a student I want to see all course offerings by semester and department so that I can 
-        choose major courses to register for */
-
-        /* As a student I want to see all courses that meet a core goal, so that I can plan out
-           my courses over the next few semesters and choose core courses that make sense for me */
-
-        /* As a student I want to find a course that meets two different core goals, so that I can
-        "feed two birds with one seed" (save time by taking one class that will fulfill two 
-          requirements */
-
-        /* As a freshman adviser, I want to see all the core goals which do not have any course offerings 
-           for a given semester, so that I can work with departments to get some courses offered
-           that students can take to meet those goals */
-
         
      }
 }
+
