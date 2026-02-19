151a63f57fbeba9cde867bf6441f155c528fd86d
Jordan Stair <jdstair@coastal.edu>
2026-02-18 20:51:41 -0500

    Finished User Story 3

--- CourseServices.cs
+++ CourseServices.cs
@@ -28,8 +28,6 @@ namespace cs330_proj1
         /* As a student, I want to see all course offerings by semester, so that I can choose from what's
            available to register for next semester */
 
-         /* START HERE: LEFT OFF HERE ON USER STORY 3 */
-
          public List <CourseOffering> getCourseOfferingsBySemester(String semester) {
           List<CourseOffering> allCourseOfferings = new List<CourseOffering>();
          foreach (CourseOffering co in repo.Offerings) {
