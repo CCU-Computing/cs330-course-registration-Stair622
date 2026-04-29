799cee05f8835110730c365be2751281edce0906
Jordan Stair <jdstair@coastal.edu>
2026-04-28 22:23:15 -0400

    Finished Unit Tests for User Story 2

--- CourseProject.Tests/CourseServicesTests.cs
+++ CourseProject.Tests/CourseServicesTests.cs
@@ -44,10 +44,52 @@ public class CourseServicesTests
 
 
     // 3. As a student, I want to see all course offerings by semester, so that I can choose from what's available to register for next semester.
+    [Fact]
+    public void GetCourseOfferingsBySemester_OneOfferingInSemester_ReturnsOneOffering() {
+        // Arrange
+        var mockRepository = new Mock<ICourseRepository>();
+        mockRepository.Setup(m => m.Offerings).Returns(new List<CourseOffering>() {
+            new CourseOffering() {
+                Section = "1",
+                Semester = "Spring 2021",
+                TheCourse = GetTestCourses().First()
+            }
+        });
 
+        var courseServices = new CourseServices(mockRepository.Object);
+
+        // Act
+        var result = courseServices.getCourseOfferingsBySemester("Spring 2021");
+
+        // Assert
+        var itemInList = Assert.Single(result);
+        Assert.Equal("Spring 2021", itemInList.Semester);
+    }
+
+    [Fact]
+    public void GetCourseOfferingsBySemester_NoOfferingsInSemester_ReturnsEmptyList() {
+        // Arrange
+        var mockRepository = new Mock<ICourseRepository>();
+        mockRepository.Setup(m => m.Offerings).Returns(new List<CourseOffering>() {
+            new CourseOffering() {
+                Section = "1",
+                Semester = "Fall 2020",
+                TheCourse = GetTestCourses().First()
+            }
+        });
+
+        var courseServices = new CourseServices(mockRepository.Object);
+
+        // Act
+        var result = courseServices.getCourseOfferingsBySemester("Spring 2021");
+
+        // Assert
+        Assert.Empty(result);
+    }
 
 
     // 4. As a student I want to see all course offerings by semester and department so that I can choose major courses to register for. 
+
 
     private List<Course> GetTestCourses()
         {
